223f88f6ebd038feec005dc49cb5487ef457f34f
Faraz_Salehi <faraz67fz@gmail.com>
2021-07-11 11:57:38 +0430

    created config helper

---
+++ src/Helpers/Config.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Helpers;
+
+use App\Exceptions\ConfigFileNotFoundExeption;
+
+class Config
+{
+    public static function getFileContents(string $filename)
+    {
+        $filepath = realpath(__DIR__."/../Configs/{$filename}.php");
+
+        if(!$filepath) {
+            throw new ConfigFileNotFoundExeption();
+        }
+
+        return require_once $filepath;
+    }
+
+    public static function get(string $filename,$key = null)
+    {
+        $filecontents = self::getFileContents($filename);
+
+        if(is_null($key)) return $filecontents;
+
+        return $filecontents[$key] ?? null;
+    }
+}
+
