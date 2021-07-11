2db1a92bb329e0b820efc07e4aff0a98d1e7ca3c
Faraz_Salehi <faraz67fz@gmail.com>
2021-07-11 11:57:15 +0430

    created db driver configs

---
+++ src/Configs/database.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Configs;
+
+return [
+    'pdo' => [
+        'driver' => 'mysql',
+        'host' => '127.0.0.1',
+        'database' => 'php_tdd_orm',
+        'db_user' => 'root',
+        'db_password' => '123456',
+    ],
+];
