3618cca46ef2bee73498ce3484accae66356e46d
Faraz_Salehi <faraz67fz@gmail.com>
2021-07-13 12:44:56 +0430

    added invalid pdo config for test

--- src/Configs/database.php
+++ src/Configs/database.php
@@ -8,13 +8,20 @@ return [
         'host' => '127.0.0.1',
         'database' => 'php_tdd_orm',
         'db_user' => 'root',
-        'db_password' => '123456',
+        'db_password' => '',
     ],
     'pdo_testing' => [
         'driver' => 'mysql',
         'host' => '127.0.0.1',
         'database' => 'php_tdd_orm_testing',
         'db_user' => 'root',
-        'db_password' => '123456',
+        'db_password' => '',
     ],
+    'pdo_invalid' =>[
+        'driver' => 'mysql',
+        'host' => '127.0.0.1',
+        'database' => 'foo_database',
+        'db_user' => 'roo00t',
+        'db_password' => '2222',
+    ]
 ];
