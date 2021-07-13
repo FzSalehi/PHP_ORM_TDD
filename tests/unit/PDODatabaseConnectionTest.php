80ec4c8d628ec558e0277956cdabe38adfb21eab
Faraz_Salehi <faraz67fz@gmail.com>
2021-07-13 12:47:08 +0430

    created tests for pdo database connectino

--- tests/unit/PDODatabaseConnectionTest.php
+++ tests/unit/PDODatabaseConnectionTest.php
@@ -4,7 +4,9 @@ namespace Tests\unit;
 
 use App\Contracts\DatabaseConnectionInterface;
 use App\Database\PDODatabaseConnection;
+use App\Exceptions\PdoDatabaseConnectionException;
 use App\Helpers\Config;
+use PDO;
 use PHPUnit\Framework\TestCase;
 
 class PDODatabaseConnectionTest extends TestCase
@@ -20,9 +22,38 @@ class PDODatabaseConnectionTest extends TestCase
 
         $this->assertInstanceOf(DatabaseConnectionInterface::class,$pdodatabaseconnection);
     }
+    /**
+     * @test
+     */
+    public function connectMethodShouldConnectToDB()
+    {
+        $pdo = new PDODatabaseConnection($this->getConfig());
+        
+        $pdo->connect();
+
+        $this->assertInstanceOf(PDO::class,$pdo->getConnection());
+    }
+
+    /**
+     * @test
+     */
+
+     public function ifPdoConnectionFailsShouldThrowPdoDatabaseConnectionException()
+     {
+        $this->expectException(PdoDatabaseConnectionException::class);
+        
+        $pdo = new PDODatabaseConnection($this->getWrongConfig());
+        
+        $pdo->connect();
+     }
 
     private function getConfig()
     {
         return Config::get('database', 'pdo_testing');
+    }
+
+    private function getWrongConfig()
+    {
+        return Config::get('database' , 'pdo_invalid');
     }
 }
