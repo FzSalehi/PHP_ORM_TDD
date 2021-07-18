4deff1a738812df1e48b8275adc26a18865b1a9b
Faraz_Salehi <faraz67fz@gmail.com>
2021-07-18 17:07:50 +0430

    created update query builder.

--- src/Database/PDOQueryBuilder.php
+++ src/Database/PDOQueryBuilder.php
@@ -10,6 +10,10 @@ class PDOQueryBuilder
 
     protected $table;
 
+    protected $conditions;
+
+    protected $values;
+
     public function __construct(DatabaseConnectionInterface $pdoconnection)
     {
         $this->pdo = $pdoconnection->getConnection();
@@ -39,7 +43,38 @@ class PDOQueryBuilder
         $query = $this->pdo->prepare($sql);
 
         $query->execute(array_values($data));
-       
+
         return (int) $this->pdo->lastInsertId();
+    }
+
+    public function where(string $column, string $value)
+    {
+        $this->conditions[] = "{$column} = ?";
+
+        $this->values[] = $value;
+
+        return $this;
+    }
+
+    public function update(array $data)
+    {
+        $fields = [];
+
+        foreach ($data as $column => $value) {
+            $fields[] = "{$column} = '{$value}'";
+        }
+        
+        $fields = implode(',', $fields);
+
+        $conditions = implode(' and ' , $this->conditions);
+
+        $sql = "UPDATE {$this->table} SET {$fields} WHERE {$conditions}";
+        
+
+        $query = $this->pdo->prepare($sql);
+
+        $query->execute($this->values);
+
+        return $query->rowCount();
     }
 }
