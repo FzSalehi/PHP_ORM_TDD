aa3931bcaf640bd324c8093c6b94a196b97dbec3
Faraz_Salehi <faraz67fz@gmail.com>
2021-07-18 17:07:27 +0430

    Refactor & created test to update a table

--- tests/unit/PDOQueryBuilderTest.php
+++ tests/unit/PDOQueryBuilderTest.php
@@ -10,35 +10,73 @@ use PHPUnit\Framework\TestCase;
 class PDOQueryBuilderTest extends TestCase
 {
     // Testing for CRUD :
+    private $pdo;
 
+    private $queryBuilder;
+
+    public function setUp() : void
+    {
+        $this->pdo = new PDODatabaseConnection($this->getConfig());
+
+        $this->pdo->connect();
+
+        $this->queryBuilder = new PDOQueryBuilder($this->pdo);
+
+
+    }
     /**
      * @test
      */
     public function itCanCreateData()
     {
-        $pdodatabaseconnection = new PDODatabaseConnection($this->getConfig());
-
-        $pdodatabaseconnection->connect();
         
-        $queryBuilder = new PDOQueryBuilder($pdodatabaseconnection);
+        $result = $this->insertIntoDB();
+
+        $this->assertIsInt($result);
+
+        $this->assertGreaterThan(0,$result);
+
+        return $result;
+    }
+
+    /**
+     * @test
+     * @depends itCanCreateData
+     */
+    public function itCanUpdateExistedData($id)
+    {
+
+        $data = [
+            'text' => 'some other text',
+            'link' => 'http://link.fz2',
+        ];
+
+        $result = $this->queryBuilder
+                        ->table('bugs')
+                        ->where('id',$id)
+                        ->where('text','some text')
+                        ->update($data);
+
+        $this->assertEquals(1, $result);
+
+    }
+
+    private function insertIntoDB()
+    {
         
         // we are assuming that user wants to report a bug for a linked page
         $data = [
             'title' => 'bug title',
             'text' => 'some text',
             'link' =>  'http://link.fz',
-            'user_id' => 1,
+            'user_id' => rand(2,10),
         ];
 
-        $result = $queryBuilder->table('bugs')->create($data);
-
-        $this->assertIsInt($result);
-
-        $this->assertGreaterThan(0,$result);
+        return $this->queryBuilder->table('bugs')->create($data);
     }
-
     private function getConfig()
     {
         return Config::get('database', 'pdo_testing');
     }
+
 }
